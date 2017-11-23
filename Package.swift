7a1ac600a962ac36975ce563b725975b53460ec1
Ivan Dilchovski <idtechlight@gmail.com>
2017-11-24 01:50:43 +0200

    Update to Swift 4

--- Package.swift
+++ Package.swift
@@ -9,16 +9,22 @@ let package = Package(
         .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.4.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/vapor/mysql-provider", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/vapor/jwt.git", .upToNextMajor(from: "2.3.0")),
+        .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.2.0"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider"],
+        .target(name: "App",
+                dependencies: ["Vapor", "FluentProvider", "MySQLProvider", "AuthProvider", "JWT"],
                 exclude: [
                     "Config",
+                    "Database",
+                    "Localization",
                     "Public",
                     "Resources",
-                ]),
+                    ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
